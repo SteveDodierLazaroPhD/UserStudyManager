d719e66bcd1c344c2a84e5bcafbb8f81a911171e
Steve Dodier-Lazaro <sidnioulz@gmail.com>
2015-03-06 10:53:59 +0000

    Useless studySetup function in main...
    original bzr status:
    revno: 6
    committer: Steve Dodier-Lazaro <s.dodier-lazaro@cs.ucl.ac.uk>
    branch nick: UCLStudyClient
    timestamp: Fri 2015-03-06 10:53:59 +0000

--- main.cpp
+++ main.cpp
@@ -1,8 +1,20 @@
 #include "mainwindow.h"
 #include "study.h"
+#include "part.h"
+#include "study.h"
 #include <QApplication>
 #include <QTranslator>
 #include <QLibraryInfo>
+#include <iostream>
+
+using namespace std;
+
+/**
+ * @brief studySetup Use this function to configure your study
+ */
+static void studySetup()
+{
+}
 
 int main(int argc, char *argv[])
 {
@@ -15,6 +27,8 @@ int main(int argc, char *argv[])
     QTranslator myappTranslator;
     myappTranslator.load("study_" + QLocale::system().name());
     a.installTranslator(&myappTranslator);
+
+    studySetup();
 
     MainWindow w;
     w.show();
